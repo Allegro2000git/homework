180dc9b66ecc537d0a61beb4603e0a1c20fa4ca8
Oleg Kova <okovalchuk722@gmail.com>
2025-03-21 15:48:41 +0300

    hw 2.3 last

--- src/s2-homeworks/hw03/Greeting.tsx
+++ src/s2-homeworks/hw03/Greeting.tsx
@@ -24,7 +24,7 @@ const Greeting: React.FC<GreetingPropsType> = (
         lastUserName,
     }
 ) => {
-    const inputClass = s.errorInput ? s.errorInput : s.input
+    const inputClass = error? s.errorInput : s.input
 
     return (
         <div id={'hw3-form'} className={s.greetingForm}>
